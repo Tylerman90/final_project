ae744719bf546dd99189912a88938083c014aec3
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-03-28 15:40:07 -0400

    added photos

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -5,8 +5,6 @@ function initMap() {
   });
 
   var infoWindow = new google.maps.InfoWindow({map: map});
-
-
   var markers = [];
 
 
@@ -328,29 +326,29 @@ function initMap() {
     infoWindow.open(map, marker)
   };
 
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
 
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-      };
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     var pos = {
+//       lat: position.coords.latitude,
+//       lng: position.coords.longitude
+//     };
+
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent('Location found.');
+//     map.setCenter(pos);
+//   }, function() {
+//     handleLocationError(true, infoWindow, map.getCenter());
+//   });
+// } else {
+//           handleLocationError(false, infoWindow, map.getCenter());
+//         }
+//       }
+
+//       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent(browserHasGeolocation ?
+//           'Error: The Geolocation service failed.' :
+//           'Error: Your browser doesn\'t support geolocation.');
+};
